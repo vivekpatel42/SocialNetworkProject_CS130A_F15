0a81dd2470f4b0bcb4ef2d9fdae9703537e30406
Vivek Patel <vivekp42095@gmail.com>
2015-10-10 21:13:19 -0700

    Added implementation for functions in cpp files

--- DoubleLinkedList.cpp
+++ DoubleLinkedList.cpp
@@ -1,1 +1,40 @@
 #include "DoubleLinkedList.h"
+
+DoubleLinkedList::DoubleLinkedList() { }
+
+DoubleLinkedList::ListNode* getHead() const
+{
+	return this -> head;
+}
+
+DoubleLinkedList::void setHead(ListNode* l)
+{
+	// Not yet fully implemented!!!
+	
+	if (head) 
+	{
+		ListNode*
+	}
+	this -> head = l;
+}
+
+DoubleLinkedList::ListNode* getItem(int n) const
+{
+	ListNode* result = this -> head;
+	while (n != 0)
+	{
+		result = result -> getNext();
+		n--;
+	}
+	return result;
+}
+
+DoubleLinkedList::void setItem(int n, ListNode* l)
+{
+	if (n == 0)
+	{
+		this.setHead(l);
+		return;
+	}
+
+}	
--- ListNode.cpp
+++ ListNode.cpp
@@ -1,2 +1,33 @@
 #include "ListNode.h"
 
+ListNode::ListNode() { }
+
+ListNode::const T& getData() const 
+{
+	return this.T;
+}
+
+ListNode::void setData(const T& value) 
+{
+	this.T = value;
+}
+
+ListNode::ListNode* getNext() const 
+{
+	return this -> next;
+}
+
+ListNode::void setNext(ListNode* l)
+{
+	this -> next = l;
+}
+
+ListNode::ListNode* getPrev() const
+{
+	return this -> prev;
+}
+
+ListNode::void setPrev(ListNode* l)
+{
+	this -> prev = l;
+}
